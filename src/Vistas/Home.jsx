058c00f262ac1925b44245fa2e1dbc63ae02dc18
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-05-14 22:44:33 -0300

    Versión inicial del home terminada

--- src/Vistas/Home.jsx
+++ src/Vistas/Home.jsx
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react';
+import LinkedinLogo from '../imagenes/linkedin.svg';
+import GitHubLogo from '../imagenes/github.svg';
 
 
 export default function Home() {
@@ -10,18 +12,20 @@ export default function Home() {
                         <h2>Hola! Mi nombre es Matias, y soy un frontend developer</h2>
                     </div>
                 </section>
-                <section>
-                    <div>
-                        <h3>Tengo conocimientos en:</h3>
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>Maquetación mobile first</li>
-                            <li>Responsive design</li>
-                            <li>React.js</li>
-                            <li>Ingles</li>
-                        </ul>
+                <section className="d-flex flex-column justify-content-center align-items-center ps-3 pe-3 text-center">
+                    <h5 className="mb-3">Bienvenido/a a mi primer projecto con React.js y Bootstrap. Acá vas a encontrar demostraciones de lo que puedo hacer hasta el momento.</h5>
+                    <p>Busco seguir capacitándome en Desarrollo Web Frontend, colaborar en una posición acorde a esos conocimientos, y, eventualmente, capacitarme también en Backend y convertirme en un Fullstack Developer. <br />
+                    Tengo conocimientos y habilidades en responsive design, maquetación mobile first, Git y GitHub, e ingles.
+                    Actualmente estoy aprendiendo React.js con componentes funcionales y Hooks.</p>
+                </section>
+                <section className="d-flex justify-content-around">
+                    <div className="d-flex flex-column justify-content-center align-items-center">
+                        <a href="https://www.linkedin.com/in/matias-d-3094b8208"><img src={LinkedinLogo} alt="Logo de Linkedin" className="icon-height"/></a>
+                        <h5>Linkedin</h5>
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-center">
+                        <a href="https://github.com/mati-77/React-Playground"><img src={GitHubLogo} alt="Logo de GitHub" className="icon-height"/></a>
+                        <h5>GitHub</h5>
                     </div>
                 </section>
             </main>
